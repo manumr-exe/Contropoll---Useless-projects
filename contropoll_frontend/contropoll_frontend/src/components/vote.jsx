edd9e7bd06cdf6ebc6ab8fc9be619851f611f902
rithin <88842423+RithinM69@users.noreply.github.com>
2024-11-02 23:28:20 +0530

    Update vote.jsx

--- contropoll_frontend/contropoll_frontend/src/components/vote.jsx
+++ contropoll_frontend/contropoll_frontend/src/components/vote.jsx
@@ -7,12 +7,13 @@ export default function Vote({ text, percentage = 0, votes = 0, onVoteSelect }) 
                 name="vote"
                 value={text}
                 id={text}
-                onChange={onVoteSelect} // Handling vote selection
-                aria-label={`Vote for ${text}`} // Improved accessibility
+                onChange={onVoteSelect}
+                aria-label={`Vote for ${text}`}
             />
             <label
                 htmlFor={text}
-                className="bg-white block rounded border-4 border-transparent cursor-pointer shadow-lg p-6 transition duration-200 hover:border-blue-400"
+                className="bg-white block rounded border-4 border-transparent cursor-pointer shadow-lg p-6 transition duration-200 
+                hover:border-blue-400 checked:border-blue-500 checked:bg-blue-50" // Classes for visual feedback
             >
                 <p className="text-2xl font-bold flex items-center justify-between">
                     {text}
